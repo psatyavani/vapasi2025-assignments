a6cd0a5b749a9bcf825becd50ef4a749f56d5194
satyavanipothukuchi <pothukuchi.satyavani@gmail.com>
2025-06-30 19:07:10 +0530

    Updated assignments for week 1 java projects

--- week-2-java/src/main/java/com/thoughtworks/exceptions/MyCalculationApp.java
+++ week-2-java/src/main/java/com/thoughtworks/exceptions/MyCalculationApp.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class MyCalculationApp {
 public static void main(String[] args){
-    Scanner scanner=new Scanner(System.in);
-    Calculator cal=new Calculator();
+        Scanner scanner=new Scanner(System.in);
+        Calculator cal=new Calculator();
 
         System.out.println("Input number:");
-    int input= scanner.nextInt();
+        int input= scanner.nextInt();
         try {
         double result = cal.caldouble(input);
         System.out.println("Calculated result:"+result) ;
